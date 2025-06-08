fe31d2bbd928cd5690b40e786fdbefcefb04710f
cihatemrevrds <emrevardis@gmail.com>
2025-06-08 15:12:54 +0300

    asset

--- lib/widgets/menu_button.dart
+++ lib/widgets/menu_button.dart
@@ -4,32 +4,44 @@ class MenuButton extends StatelessWidget {
   final String text;
   final VoidCallback? onPressed;
 
-  const MenuButton({
-    super.key, 
-    required this.text, 
-    required this.onPressed,
-  });
-
+  const MenuButton({super.key, required this.text, required this.onPressed});
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: 250,
       height: 60,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color(0xFF4E2C0B),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          image: const DecorationImage(
+            image: AssetImage("assets/images/backgrounds/button_wood.jpg"),
+            fit: BoxFit.cover,
           ),
-          elevation: 4,
         ),
-        child: Text(
-          text,
-          style: const TextStyle(
-            fontFamily: 'Rye',
-            fontSize: 20,
-            color: Colors.white,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            elevation: 4,
+          ),
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontFamily: 'Rye',
+              fontSize: 20,
+              color: Colors.white,
+              shadows: [
+                Shadow(
+                  offset: Offset(1, 1),
+                  blurRadius: 2,
+                  color: Colors.black,
+                ),
+              ],
+            ),
           ),
         ),
       ),
